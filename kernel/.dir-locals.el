027a6fe904bfe4edaa75084ef04065b57e9849c5
Eemeli <eemeli.o.lehtonen@utu.fi>
2022-12-27 18:56:23 +0200

    Revert "bootloader + bootloader_api"
    This reverts commit cbbe3e18c8d68e082a85269d85ca8b7182a9cb5d.

--- kernel/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((lsp-rust-analyzer-cargo-target . "x86_64-unknown-none")
-         (lsp-rust-all-targets . nil)
-         (lsp-rust-analyzer-cargo-all-targets . nil))))
--- os/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((lsp-rust-analyzer-cargo-target . nil)
-         (lsp-rust-all-targets . nil)
-         (lsp-rust-analyzer-cargo-all-targets . nil))))
