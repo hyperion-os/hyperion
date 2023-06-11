8a81d4accb5bbcef6a5b7af2b6ee24508b1d7b04
Eemeli <eemeli.o.lehtonen@utu.fi>
2023-06-11 20:12:08 +0300

    fix: log level cmp

--- crates/log-multi/src/lib.rs
+++ crates/log-multi/src/lib.rs
@@ -6,7 +6,7 @@ use core::fmt::Arguments;
 
 use crossbeam::atomic::AtomicCell;
 use hyperion_boot::args;
-use hyperion_log::{println, set_logger, LogLevel, Logger};
+use hyperion_log::{set_logger, LogLevel, Logger};
 
 //
 
@@ -45,7 +45,7 @@ const _: () = assert!(AtomicCell::<LogLevel>::is_lock_free());
 
 impl Logger for MultiLogger {
     fn is_enabled(&self, level: LogLevel) -> bool {
-        self.qemu.load() <= level
+        self.qemu.load() >= level
     }
 
     fn print(&self, _level: LogLevel, args: Arguments) {
