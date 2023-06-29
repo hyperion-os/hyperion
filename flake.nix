71c4d50d45bbca2c170db5c07130350f0205cd76
Eemeli <oneleh@utu.fi>
2023-06-29 04:00:32 +0300

    nixos dev env flake

--- flake.nix
+++ flake.nix
@@ -1,35 +1,37 @@
 {
-    description = "Nix dev-env";
+  description = "Hyperion Nix devenv";
 
-    inputs = {
-        nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-        rust-overlay.url = "github:oxalica/rust-overlay";
-        flake-utils.url  = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }: {
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                overlays = [ (import rust-overlay) ];
-                pkgs = import nixpkgs {
-                    inherit system overlays;
-                };
-            in with pkgs; {
-                devShells.default = mkShell {
-                    buildInputs = [
-                    openssl
-                    pkg-config
-                    exa
-                    fd
-                    rust-bin.nightly.latest.default
-                ];
-
-                shellHook = ''
-                    alias ls=exa
-                    alias find=fd
-                '';
-                };
-            }
-        );
-    };
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.nightly.latest.default;
+          rustc = pkgs.rust-bin.nightly.latest.default;
+        };
+      in
+      {
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "x86_64-unknown-none" ];
+            })
+            cargo-udeps
+          ];
+        };
+      }
+    );
 }
