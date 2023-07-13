bad9cbec71ce5e888f1f6f088061c71e092045e9
Eemeli <oneleh@utu.fi>
2023-07-14 01:58:44 +0300

    syscall lib

--- crates/sample-elf/src/main.rs
+++ crates/sample-elf/src/main.rs
@@ -8,32 +8,13 @@ pub extern "C" fn _start() -> ! {
     let null_ptr = core::hint::black_box(0x0) as *const u8;
     core::hint::black_box(unsafe { *null_ptr }); */
 
-    loop {
-        trigger_syscall(42, 43, 44, 0, 45, 46);
-    }
+    hyperion_syscall::log("testing");
+
+    loop {}
 }
 
 pub fn main(args: &[&str]) -> Result<(), i64> {
     Ok(())
-}
-
-pub fn trigger_old_syscall(syscall_num: u32) {
-    unsafe {
-        core::arch::asm!("int 0xAA", in("eax") syscall_num);
-    }
-}
-
-pub extern "C" fn trigger_syscall(
-    _rdi: u64,
-    _rsi: u64,
-    _rdx: u64,
-    _rcx_ignored: u64,
-    _r8: u64,
-    _r9: u64,
-) {
-    unsafe {
-        core::arch::asm!("syscall");
-    }
 }
 
 #[panic_handler]
---
+++ crates/syscall/src/lib.rs
@@ -0,0 +1,49 @@
+#![no_std]
+
+#[inline(always)]
+pub fn log(str: &str) {
+    unsafe {
+        // TODO: should null terminated strings be used instead to save registers?
+        // decide later™
+        trigger_syscall(1, str.as_ptr() as u64, str.len() as u64, 0, 0, 0);
+    }
+}
+
+#[inline(always)]
+pub unsafe extern "C" fn trigger_syscall(
+    syscall_id: u64,
+    arg0: u64,
+    arg1: u64,
+    arg2: u64,
+    arg3: u64,
+    arg4: u64,
+) -> u64 {
+    let result: u64;
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rax") syscall_id,
+            in("rdi") arg0,
+            in("rsi") arg1,
+            in("rdx") arg2,
+            in("r8") arg3,
+            in("r9") arg4,
+            lateout("rax") result
+        );
+    }
+    result
+}
+
+/* #[inline(always)]
+pub extern "C" fn trigger_syscall(
+    _rdi: u64,
+    _rsi: u64,
+    _rdx: u64,
+    _rcx_ignored: u64,
+    _r8: u64,
+    _r9: u64,
+) {
+    unsafe {
+        core::arch::asm!("syscall");
+    }
+} */
