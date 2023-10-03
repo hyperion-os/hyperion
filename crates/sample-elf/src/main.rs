612ef901ae77908bf710c6856f4518d9a82833b9
Eemeli <oneleh@utu.fi>
2023-10-03 03:17:24 +0300

    sample elf cleanup

--- crates/sample-elf/src/main.rs
+++ crates/sample-elf/src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(format_args_nl)]
 
 use core::fmt::{self, Write};
 
@@ -7,49 +8,57 @@ use hyperion_syscall::*;
 
 //
 
-struct SyscallLog;
+pub fn main() {
+    println!("sample app main");
+
+    let mut next = timestamp().unwrap() as u64;
+    for i in 0.. {
+        nanosleep_until(next);
+        next += 1_000_000_000;
+
+        println!("seconds since boot: {i}");
+    }
+}
 
 //
 
-impl Write for SyscallLog {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        if hyperion_syscall::log(s) == 0 {
-            Ok(())
-        } else {
-            Err(fmt::Error)
+#[macro_export]
+macro_rules! println {
+    ($($v:tt)*) => {
+        _print(format_args_nl!($($v)*));
+    };
+}
+
+//
+
+fn _print(args: fmt::Arguments) {
+    struct SyscallLog;
+
+    //
+
+    impl Write for SyscallLog {
+        fn write_str(&mut self, s: &str) -> fmt::Result {
+            if hyperion_syscall::log(s) == 0 {
+                Ok(())
+            } else {
+                Err(fmt::Error)
+            }
         }
     }
+
+    _ = SyscallLog.write_fmt(args);
 }
 
 //
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    writeln!(
-        &mut SyscallLog,
-        "sample app main\nints enabled?: {}",
-        x86_64::instructions::interrupts::are_enabled()
-    )
-    .unwrap();
-
-    // page fault test:
-    /* writeln!(&mut SyscallLog, "sample-elf page fault test").unwrap();
-    let null_ptr = core::hint::black_box(0x0) as *const u8;
-    core::hint::black_box(unsafe { *null_ptr }); */
-
-    let mut next = 0;
-    for i in 0.. {
-        nanosleep_until(next);
-        next += 1_000_000_000;
-
-        writeln!(&mut SyscallLog, "testing `{i}`").unwrap();
-    }
-
+extern "C" fn _start() -> ! {
+    main();
     exit(0);
 }
 
 #[panic_handler]
-fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    log("panic");
+fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    println!("{info}");
     exit(-1);
 }
