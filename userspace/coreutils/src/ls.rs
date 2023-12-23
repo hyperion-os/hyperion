36457f59fdeba6ffb9cb75057f62c9c7cdb4e892
Eemeli <oneleh@utu.fi>
2023-12-23 17:28:52 +0200

    ls cmd use '/' if no args are given

--- userspace/coreutils/src/ls.rs
+++ userspace/coreutils/src/ls.rs
@@ -1,15 +1,14 @@
 use alloc::format;
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use hyperion_num_postfix::NumberPostfix;
 use libstd::{fs::Dir, print, println};
 
 //
 
 pub fn cmd<'a>(mut args: impl Iterator<Item = &'a str>) -> Result<()> {
-    let a1 = args
-        .next()
-        .ok_or_else(|| anyhow!("expected at least one argument"))?;
+    let a1 = args.next().unwrap_or("/"); // TODO: cwd
+                                         // .ok_or_else(|| anyhow!("expected at least one argument"))?;
 
     let mut dir = Dir::open(a1).unwrap();
 
