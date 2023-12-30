0d350ea13913256378e0789efd462e6441c085ef
xor-bits <xor-bits@users.noreply.github.com>
2023-12-30 14:45:13 +0000

    deploy: 9ef816e9cdfb6f771f769644608de0416fa29d8f

--- trait.impl/hyperion_vfs/device/trait.FileDevice.js
+++ trait.impl/hyperion_vfs/device/trait.FileDevice.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "hyperion_driver_acpi":[["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_acpi/hpet/struct.HpetDevice.html\" title=\"struct hyperion_driver_acpi::hpet::HpetDevice\">HpetDevice</a>"]],
-"hyperion_driver_framebuffer":[["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_framebuffer/struct.FboDevice.html\" title=\"struct hyperion_driver_framebuffer::FboDevice\">FboDevice</a>"],["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_framebuffer/struct.FboInfoDevice.html\" title=\"struct hyperion_driver_framebuffer::FboInfoDevice\">FboInfoDevice</a>"]],
+"hyperion_driver_framebuffer":[["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_framebuffer/struct.FboInfoDevice.html\" title=\"struct hyperion_driver_framebuffer::FboInfoDevice\">FboInfoDevice</a>"],["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_framebuffer/struct.FboDevice.html\" title=\"struct hyperion_driver_framebuffer::FboDevice\">FboDevice</a>"]],
 "hyperion_driver_rtc":[["impl FileDevice for <a class=\"struct\" href=\"hyperion_driver_rtc/struct.RtcDevice.html\" title=\"struct hyperion_driver_rtc::RtcDevice\">RtcDevice</a>"]],
 "hyperion_kernel_impl":[["impl FileDevice for <a class=\"struct\" href=\"hyperion_kernel_impl/struct.BoundSocket.html\" title=\"struct hyperion_kernel_impl::BoundSocket\">BoundSocket</a>"]],
 "hyperion_vfs":[]
