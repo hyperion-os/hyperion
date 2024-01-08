0b054a1f282b416f0cb1dfc5202533ede43903bc
Eemeli <oneleh@utu.fi>
2024-01-08 03:10:59 +0200

    ring buf progress

--- crates/ring/src/lib.rs
+++ crates/ring/src/lib.rs
@@ -5,20 +5,11 @@
 
 extern crate alloc;
 
-use core::cmp;
-
-use crossbeam::utils::CachePadded;
-use sync::*;
-
 //
 
-pub(crate) mod sync {
-    #[cfg(not(all(loom, not(target_os = "none"))))]
-    pub use core::sync::atomic::{AtomicUsize, Ordering};
+use core::sync::atomic::{AtomicUsize, Ordering};
 
-    #[cfg(all(loom, not(target_os = "none")))]
-    pub use loom::sync::atomic::{AtomicUsize, Ordering};
-}
+use crossbeam::utils::CachePadded;
 
 //
 
@@ -85,32 +76,24 @@ impl Slot {
 /// [`RingBufMarker::consume`] after [`RingBufMarker::consume`]
 /// invalidates the first consumed slot
 #[derive(Debug)]
+#[repr(C)]
 pub struct RingBufMarker {
     read: CachePadded<AtomicUsize>,
     write: CachePadded<AtomicUsize>,
-    len: CachePadded<AtomicUsize>,
+    // len: CachePadded<AtomicUsize>,
     capacity: usize,
 }
 
 impl RingBufMarker {
-    #[cfg(not(all(loom, not(target_os = "none"))))]
     #[must_use]
     pub const fn new(len: usize) -> Self {
-        Self {
-            read: CachePadded::new(AtomicUsize::new(0)),
-            write: CachePadded::new(AtomicUsize::new(0)),
-            len: CachePadded::new(AtomicUsize::new(0)),
-            capacity: len,
-        }
-    }
+        assert!(len != 0);
 
-    #[cfg(all(loom, not(target_os = "none")))]
-    #[must_use]
-    pub fn new(len: usize) -> Self {
         Self {
             read: CachePadded::new(AtomicUsize::new(0)),
             write: CachePadded::new(AtomicUsize::new(0)),
-            len,
+            // len: CachePadded::new(AtomicUsize::new(0)),
+            capacity: len,
         }
     }
 
@@ -118,6 +101,7 @@ impl RingBufMarker {
     pub fn uninit_slot(&self) -> Slot {
         let write = self.write.load(Ordering::Acquire);
         let read = self.read.load(Ordering::Acquire);
+        // let len = self.len.load(Ordering::Acquire);
 
         // read end - 1 is the limit, the number of available spaces can only grow
         // read=write would be ambiguous so read=write always means that the whole buf is empty
@@ -125,25 +109,35 @@ impl RingBufMarker {
         let avail = if write < read {
             read - write
         } else {
-            self.capacity - read + write
+            self.capacity - write + read
         };
+        assert!(avail <= self.capacity);
 
         Slot::new(write, avail - 1)
+        // Slot::new(
+        //     write,
+        //     self.capacity.checked_sub(len).unwrap_or_else(|| {
+        //         panic!("uninit_slot_panic({write} {len} {})", self.capacity);
+        //     }),
+        // )
     }
 
     #[must_use]
     pub fn init_slot(&self) -> Slot {
         let read = self.read.load(Ordering::Acquire);
         let write = self.write.load(Ordering::Acquire);
+        // let len = self.len.load(Ordering::Acquire);
 
         // write end is the limit, the number of available items can only grow
         let avail = if read <= write {
             write - read
         } else {
-            self.capacity - write + read
+            self.capacity - read + write
         };
+        assert!(avail <= self.capacity);
 
         Slot::new(read, avail)
+        // Slot::new(read, len)
     }
 
     #[must_use]
@@ -179,11 +173,10 @@ impl RingBufMarker {
     /// # Safety
     /// this is a write operation, see [`Self`]
     pub unsafe fn produce(&self, acquire: Slot) {
-        self.write.store(
-            (acquire.first + acquire.len) % self.capacity,
-            Ordering::Release,
-        );
-        self.len.fetch_add(acquire.len, Ordering::Release);
+        let new_write_end = (acquire.first + acquire.len) % self.capacity;
+        let old = self.write.swap(new_write_end, Ordering::Release);
+        assert_eq!(old, acquire.first);
+        // self.len.fetch_add(acquire.len, Ordering::Release);
     }
 
     /// # Safety
@@ -199,59 +192,30 @@ impl RingBufMarker {
     /// # Safety
     /// this is a read operation, see [`Self`]
     pub unsafe fn release(&self, consume: Slot) {
-        self.len.fetch_sub(consume.len, Ordering::Release);
-        self.read.store(
-            (consume.first + consume.len) % self.capacity,
-            Ordering::Release,
-        );
+        // self.len.fetch_sub(consume.len, Ordering::Release);
+        let new_read_end = (consume.first + consume.len) % self.capacity;
+        let old = self.read.swap(new_read_end, Ordering::Release);
+        assert_eq!(old, consume.first);
     }
 }
+
+//
 
 #[cfg(test)]
 mod tests {
     extern crate std;
 
-    use std::dbg;
-
-    use sync::*;
-
     use crate::RingBufMarker;
-
-    //
-
-    pub(crate) mod sync {
-        extern crate std;
-
-        #[cfg(not(all(loom, not(target_os = "none"))))]
-        pub use std::{
-            sync::{Arc, Barrier, Mutex},
-            thread,
-        };
-
-        #[cfg(all(loom, not(target_os = "none")))]
-        pub use loom::{
-            sync::{Arc, Barrier, Mutex},
-            thread,
-        };
-    }
-
-    //
-
-    macro_rules! clone_move {
-        ([$($i:ident),*] $($closure:tt)*) => {{
-            $(let $i = $i.clone();)*
-
-            move || {
-                $($closure)*
-            }
-        }};
-    }
 
     //
 
     #[test]
     fn init_empty() {
-        let marker = RingBufMarker::new(4);
+        let marker = RingBufMarker::new(5);
+
+        assert_eq!(marker.free_space(), 4);
+        assert_eq!(marker.used_space(), 0);
+
         assert_eq!(
             unsafe { marker.consume(3) },
             None,
@@ -261,118 +225,49 @@ mod tests {
 
     #[test]
     fn fill() {
-        let marker = RingBufMarker::new(4);
-        unsafe { marker.acquire(4) }.expect("the ring buf should be empty");
+        let marker = RingBufMarker::new(5);
+
+        let slot = unsafe { marker.acquire(4) }.unwrap();
+        unsafe { marker.produce(slot) };
+        assert_eq!(marker.free_space(), 0);
+        assert_eq!(marker.used_space(), 4);
     }
 
     #[test]
     fn fill_offset() {
-        let marker = RingBufMarker::new(4);
+        let marker = RingBufMarker::new(5);
 
-        assert_eq!(marker.free_space(), 3);
-        assert_eq!(marker.used_space(), 0);
-
-        let slot = unsafe { marker.acquire(2) }.expect("the ring buf should be empty");
+        let slot = unsafe { marker.acquire(2) }.unwrap();
         unsafe { marker.produce(slot) };
+        assert_eq!(marker.free_space(), 2);
+        assert_eq!(marker.used_space(), 2);
 
-        assert_eq!(marker.free_space(), 1);
+        let slot = unsafe { marker.consume(1) }.unwrap();
+        unsafe { marker.release(slot) };
+        assert_eq!(marker.free_space(), 3);
+        assert_eq!(marker.used_space(), 1);
+
+        let slot = unsafe { marker.consume(1) }.unwrap();
+        unsafe { marker.release(slot) };
+        assert_eq!(marker.free_space(), 4);
         assert_eq!(marker.used_space(), 0);
 
-        assert!(
-            unsafe { marker.consume(1) }.is_none(),
-            "the ring buf should be empty"
-        );
-
-        dbg!(&marker);
-        let slot = unsafe { marker.consume(2) }.expect("the ring buf should have 2 items");
-        dbg!(&marker);
-        unsafe { marker.release(slot) };
-
-        dbg!(&marker);
-        unsafe { marker.acquire(4) }.expect("the ring buf should be empty");
-
-        dbg!(&marker);
-        assert_eq!(
-            unsafe { marker.acquire(1) },
-            None,
-            "the ring buf should be full"
-        );
+        let slot = unsafe { marker.acquire(4) }.unwrap();
+        unsafe { marker.produce(slot) };
+        assert_eq!(marker.free_space(), 0);
+        assert_eq!(marker.used_space(), 4);
     }
 
     #[test]
-    fn loom_test() {
-        let run = || {
-            let marker = Arc::new(RingBufMarker::new(4));
-            let arr = Arc::new([const { Mutex::new(()) }; 4]);
+    fn rw() {
+        let marker = RingBufMarker::new(255);
 
-            let barrier_0 = Arc::new(Barrier::new(1));
-            let barrier_1 = Arc::new(Barrier::new(1));
+        let slot = unsafe { marker.acquire(63) }.unwrap();
+        unsafe { marker.produce(slot) };
+        assert_eq!(marker.free_space(), 191);
+        assert_eq!(marker.used_space(), 63);
 
-            assert_eq!(
-                unsafe { marker.consume(3) },
-                None,
-                "the ring buf should be empty"
-            );
-
-            let t0 = thread::spawn(clone_move! { [marker, arr, barrier_0]
-                let slot = unsafe { marker.acquire(3) }.unwrap();
-                let (a, b) = slot.slices(&arr[..]);
-                std::println!("{a:?} {b:?}");
-                for item in a.iter().chain(b) {
-                    drop(item.try_lock().unwrap());
-                }
-                unsafe { marker.produce(slot) };
-
-                barrier_0.wait();
-            });
-
-            let t1 = thread::spawn(clone_move! { [marker, arr, barrier_1]
-                let slot = unsafe { marker.acquire(3) }.unwrap();
-                let (a, b) = slot.slices(&arr[..]);
-                std::println!("{a:?} {b:?}");
-                for item in a.iter().chain(b) {
-                    drop(item.try_lock().unwrap());
-                }
-                unsafe { marker.produce(slot) };
-
-                barrier_1.wait();
-            });
-
-            let t2 = thread::spawn(clone_move! { [marker, arr, barrier_0]
-                barrier_0.wait();
-
-                let slot = unsafe { marker.consume(3) }.unwrap();
-                let (a, b) = slot.slices(&arr[..]);
-                std::println!("{a:?} {b:?}");
-                for item in a.iter().chain(b) {
-                    drop(item.try_lock().unwrap());
-                }
-                unsafe { marker.release(slot) };
-            });
-
-            let t3 = thread::spawn(clone_move! { [marker, arr, barrier_1]
-                barrier_1.wait();
-
-                let slot = unsafe { marker.consume(3) }.unwrap();
-                let (a, b) = slot.slices(&arr[..]);
-                std::println!("{a:?} {b:?}");
-                for item in a.iter().chain(b) {
-                    drop(item.try_lock().unwrap());
-                }
-                unsafe { marker.release(slot) };
-            });
-
-            t0.join().unwrap();
-            t1.join().unwrap();
-            t2.join().unwrap();
-            t3.join().unwrap();
-        };
-
-        #[cfg(not(all(loom, not(target_os = "none"))))]
-        run();
-        #[cfg(all(loom, not(target_os = "none")))]
-        loom::model(run);
-
-        panic!();
+        let slot = unsafe { marker.consume(140) };
+        assert_eq!(slot, None);
     }
 }
