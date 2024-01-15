bf9a4dbcef4c21338f52a07aecad5edc285540ca
Eemeli Lehtonen <oneleh@utu.fi>
2024-01-15 20:27:17 +0200

    remove tracing from std-test

--- userspace/std-test/src/main.rs
+++ userspace/std-test/src/main.rs
@@ -3,10 +3,6 @@ use std::{collections::HashMap, fs::OpenOptions, io::Read, path::Path};
 //
 
 fn main() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::level_filters::LevelFilter::TRACE)
-        .init();
-
     let path = Path::new("splash");
     let mut file = OpenOptions::new().read(true).open(path).unwrap();
     let mut buf = String::new();
